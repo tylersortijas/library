79fd4e2b4089d6a3b04852c73d201aae1d6ac836
tylersortijas <tylersortijas@gmail.com>
2023-02-16 19:03:47 -0800

    Added id to objects in library

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 let myLibrary = [
   {
     title: "The Lord of the Rings",
+    id: 1,
     author: "J. R. R. Tolkien",
     pages: "479",
     read: "not read yet",
@@ -15,13 +16,13 @@ const content = document.querySelector(".content");
 
 const addBookButton = document.querySelector("button");
 const cancelButton = document.querySelector("#cancel");
+const submitButton = document.querySelector("#submit");
 
 const form = document.querySelector("form");
 let titleInput = document.querySelector("#title");
 let authorInput = document.querySelector("#author");
 let pagesInput = document.querySelector("#pages");
 let readInput = document.querySelector("#read");
-let submitButton = document.querySelector("#submit");
 
 // Constructor for each book
 function Book(title, author, pages, read) {
@@ -39,6 +40,36 @@ const addBookToLibrary = () => {
   let read = readInput.value;
   let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  displayNewBook();
+  // Adds Id to any new book added 
+  myLibrary.forEach((book, i) => {
+    book.id = i + 1;
+  });
+};
+
+// Displays the newest book in Library
+const displayNewBook = () => {
+  for (let i = myLibrary.length - 1; i < myLibrary.length; i++) {
+    let book = document.createElement("div");
+    book.classList.add("card");
+    content.appendChild(book);
+
+    let title = document.createElement("h4");
+    title.textContent = `${myLibrary[i].title}`;
+    book.appendChild(title);
+    let author = document.createElement("h5");
+    author.textContent = `${myLibrary[i].author}`;
+    book.appendChild(author);
+    let pages = document.createElement("p");
+    pages.textContent = `${myLibrary[i].pages}`;
+    book.appendChild(pages);
+    let read = document.createElement("p");
+    read.textContent = `${myLibrary[i].read}`;
+    book.appendChild(read);
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    book.appendChild(removeButton);
+  }
 };
 
 // Form pop-up to add new Book
@@ -56,13 +87,19 @@ addBookButton.addEventListener("click", () => {
   openForm();
 });
 
+// Submit button to push book to Library
+submitButton.addEventListener("click", (e) => {
+  addBookToLibrary();
+  e.preventDefault();
+});
+
 // Button to close Form pop-up
 cancelButton.addEventListener("click", () => {
   closeForm();
 });
 
 // Display library array
-const displayFunction = () => {
+const initialDisplayLibrary = () => {
   for (let i = 0; i < myLibrary.length; i++) {
     let book = document.createElement("div");
     book.classList.add("card");
@@ -80,7 +117,10 @@ const displayFunction = () => {
     let read = document.createElement("p");
     read.textContent = `${myLibrary[i].read}`;
     book.appendChild(read);
+    let removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    book.appendChild(removeButton);
   }
 };
 
-displayFunction();
+initialDisplayLibrary();
